d70b3bbf3111983e2a179a8c3956b9e94bef0f76
AxelAxel97 <carlphilipsaclinton@gmail.com>
2026-04-27 09:46:07 +0200

    Create Event model and set up Django admin

--- club/models.py
+++ club/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=200)
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
